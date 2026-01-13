95203d3b67e833a3ec197b34a529c486fa0b9a0f
hakus13 <44534141+hakus13@users.noreply.github.com>
2026-01-13 01:21:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.get('/', (req, res) => {
 
 
 //Corremos el servidor en el puerto 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Aplicación corriendo en http://localhost:${port}`)
